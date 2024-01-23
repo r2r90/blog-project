93b506650a9e27791f7542eb3d2543182ee94e06
Artur Agh <aghartur@gmail.com>
2024-01-23 10:09:46 +0100

    blog & post validations added v2 empty array

--- __tests__/blog.api.test.ts
+++ __tests__/blog.api.test.ts
@@ -1,5 +1,9 @@
-describe('/blogs === BLOG API TEST', () => {
-    it('should return 200 with blogs array', () => {
+import request from "supertest";
+import { db } from "../src/db/db";
+import { app } from "../src/app";
 
-    });
-})
+describe("/blogs === BLOG API TEST", () => {
+  it("should return 200 with blogs array", async () => {
+    await request(app).get("/blogs").expect(200, db.blogsDb);
+  });
+});
--- src/db/db.ts
+++ src/db/db.ts
@@ -13,7 +13,7 @@ export type BlogType = {
 
 export const db: DBType = {
   blogsDb: [
-    {
+    /*{
       id: "1",
       name: "firstBlog",
       description:
@@ -28,6 +28,6 @@ export const db: DBType = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
       websiteUrl:
         "https://Y5Bq7PSGISmFU8n9qqN4ERMtgDenu8peSP6Ao6wN0fnGYDb6ZDnrxt.pTth7TsCyG7BPecXY6LReqDhpt6Bjc2VAy_Lp",
-    },
+    },*/
   ],
 };
