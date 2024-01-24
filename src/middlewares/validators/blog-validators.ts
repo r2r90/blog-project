72f3702868a38308c269b76b39e41a0fafdd8cca
Artur Agh <aghartur@gmail.com>
2024-01-24 13:18:48 +0100

    input validator error

--- src/middlewares/validators/blog-validators.ts
+++ src/middlewares/validators/blog-validators.ts
@@ -1,16 +1,18 @@
 import { body } from "express-validator";
+import { inputValidationMiddleware } from "../inputValidation/input-validation-middleware";
 
 export const nameValidator = body("name")
   .isString()
-  .trim()
+
   .withMessage("Incorrect Name - Name must be a string")
+  .trim()
   .isLength({ min: 1, max: 15 })
   .withMessage("Incorrect Name - min 1 character & max 15 character");
 
 export const descriptionValidator = body("description")
   .isString()
-  .trim()
   .withMessage("Incorrect description - must be a string")
+  .trim()
   .isLength({ min: 1, max: 500 })
   .withMessage("Incorrect description - min 1 character & max 500 character");
 
@@ -18,7 +20,6 @@ export const webSiteUrlValidator = body("name")
   .isString()
   .withMessage("Incorrect description - must be a string")
   .trim()
-  .withMessage("Incorrect description - must be a string")
   .isLength({ min: 1, max: 100 })
   .withMessage("Incorrect description - min 1 character & max 500 character")
   .matches(
@@ -30,4 +31,5 @@ export const blogValidation = () => [
   nameValidator,
   descriptionValidator,
   webSiteUrlValidator,
+  inputValidationMiddleware,
 ];
