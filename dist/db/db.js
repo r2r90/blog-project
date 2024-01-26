26c30f4c8e625dc4dfc3b8e9744631ee6a40b47e
Artur Agh <aghartur@gmail.com>
2024-01-26 14:58:29 +0100

    started working with mongoDb

--- dist/db/db.js
+++ dist/db/db.js
@@ -1,7 +1,30 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-exports.db = {
-    blogsDb: [],
-    postsDb: [],
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.runDb = void 0;
+const dotenv_1 = require("dotenv");
+const mongodb_1 = require("mongodb");
+(0, dotenv_1.configDotenv)();
+const uri = (_a = process.env.MONGO_URL) !== null && _a !== void 0 ? _a : "mongodb://0.0.0.0:27017";
+const client = new mongodb_1.MongoClient(uri);
+const db = client.db("blog-app");
+const runDb = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        console.log("Connected successfully to MongoDB ");
+    }
+    catch (e) {
+        console.log(`ERROR: ${e}`);
+        yield client.close();
+    }
+});
+exports.runDb = runDb;
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,19 @@
-import { DBType } from "../models/db/db-model";
+import { configDotenv } from "dotenv";
+import { MongoClient } from "mongodb";
 
-export let db: DBType = {
-  blogsDb: [],
-  postsDb: [],
+configDotenv();
+
+const uri = process.env.MONGO_URL ?? "mongodb://0.0.0.0:27017";
+const client = new MongoClient(uri);
+
+const db = client.db("blog-app");
+
+export const runDb = async () => {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB ");
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    await client.close();
+  }
 };
