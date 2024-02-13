769a3cb3f337cb144e7f48793c3925025b06017f
Artur Agh <aghartur@gmail.com>
2024-02-13 08:52:04 +0100

    testing all data users add

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from "express";
-import { blogsCollection, postsCollection } from "../db/db";
+import { blogsCollection, postsCollection, usersCollection } from "../db/db";
 
 export const testingRouter = Router();
 
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
   await blogsCollection.deleteMany({});
   await postsCollection.deleteMany({});
+  await usersCollection.deleteMany({});
   res.sendStatus(204);
 });
