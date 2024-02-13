36e8b01c91f7b028c757381611667af55c933e9b
Artur Agh <aghartur@gmail.com>
2024-02-13 08:47:37 +0100

    check user exist commented

--- src/repositories/user-repositories/user.repository.ts
+++ src/repositories/user-repositories/user.repository.ts
@@ -7,10 +7,10 @@ export class UserRepository {
     const { createdAt, login, email, passwordSalt, passwordHash } = user;
 
     // Check if User already is existing in DB
-    const existingUser = await usersCollection.findOne({
+    /*    const existingUser = await usersCollection.findOne({
       $or: [{ login }, { email }],
     });
-    if (existingUser) return null;
+    if (existingUser) return null;*/
 
     const createdUser = await usersCollection.insertOne({
       createdAt,
