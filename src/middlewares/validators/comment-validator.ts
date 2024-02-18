2bfbd53a45c1b8cf898d883b3503b1cd79893961
Artur Agh <aghartur@gmail.com>
2024-02-18 18:53:57 +0100

    comment validator

--- src/middlewares/validators/comment-validator.ts
+++ src/middlewares/validators/comment-validator.ts
@@ -1,14 +1,12 @@
 import { body } from "express-validator";
 import { inputModelValidationMiddleware } from "../inputValidation/input-model-validation-middleware";
 
-const content = body("content")
+const commentBodyValidator = body("content")
   .trim()
   .isString()
-  .not()
-  .isNumeric()
   .isLength({ min: 20, max: 300 })
   .withMessage("Incorrect comment");
 
 export const commentValidator = () => {
-  return [content];
+  return [commentBodyValidator, inputModelValidationMiddleware];
 };
