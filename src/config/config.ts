1894c35defb3661161f10f35fcbcd31c510ad300
Artur Agh <aghartur@gmail.com>
2024-02-28 14:50:34 +0100

    auth me access token

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,8 +6,8 @@ export const appConfig = {
   MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017",
   JWT_ACCESS_SECRET: process.env.JWT_SECRET || "123",
   JWT_REFRESH_SECRET: process.env.JWT_SECRET || "456",
-  JWT_ACCESS_EXPIRES_TIME: "1h",
-  JWT_REFRESH_SECRET_EXPIRES_TIME: "1h",
+  JWT_ACCESS_EXPIRES_TIME: "10s",
+  JWT_REFRESH_SECRET_EXPIRES_TIME: "20s",
   EMAIL_LOGIN: process.env.EMAIL_LOGIN,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
 };
