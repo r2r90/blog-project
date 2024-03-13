9915e686ac6febad4ceefeb9aafa936b06daf566
Artur Agh <aghartur@gmail.com>
2024-03-13 11:38:12 +0100

    get all devices 2 + (type errors)

--- src/routes/devices-router.ts
+++ src/routes/devices-router.ts
@@ -1,14 +1,19 @@
 import { Request, Response, Router } from "express";
 import { DeviceRepository } from "../repositories/device-repository/device.repository";
 import { HTTP_RESPONSE_CODES } from "../models/common";
+import { requestQuantityFixer } from "../middlewares/device-secure/requestQuantityFixer";
 
 export const devicesRouter = Router();
 
-devicesRouter.get("/", async (req: Request, res: Response) => {
-  const devicesList = await DeviceRepository.getAllDevices();
-  devicesList
-    ? res.send(devicesList).status(200)
-    : res.sendStatus(HTTP_RESPONSE_CODES.BAD_REQUEST);
-});
+devicesRouter.get(
+  "/",
+  requestQuantityFixer,
+  async (req: Request, res: Response) => {
+    const devicesList = await DeviceRepository.getAllDevices();
+    devicesList
+      ? res.send(devicesList).status(200)
+      : res.sendStatus(HTTP_RESPONSE_CODES.BAD_REQUEST);
+  }
+);
 devicesRouter.delete("/", (req, res) => {});
 devicesRouter.delete("/:id", (req, res) => {});
