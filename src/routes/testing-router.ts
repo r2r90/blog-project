2b3b74235f779e2cf6442129d8c0391e3add46f4
Artur Agh <aghartur@gmail.com>
2024-03-20 11:06:17 +0100

    lastActive aupdate when token refresh

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -5,6 +5,7 @@ import {
   commentsCollection,
   deviceConnectCollection,
   postsCollection,
+  usersCollection,
 } from "../db/db";
 
 export const testingRouter = Router();
@@ -12,8 +13,9 @@ export const testingRouter = Router();
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
   await blogsCollection.deleteMany({});
   await postsCollection.deleteMany({});
-  await commentsCollection.deleteMany({});
+  await usersCollection.deleteMany({});
   await blackListCollection.deleteMany({});
+  await commentsCollection.deleteMany({});
   await deviceConnectCollection.deleteMany({});
   await deviceConnectCollection.deleteMany({});
   res.sendStatus(204);
