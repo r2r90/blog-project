ea082c5fc9192d43b716b4aec9dc64ced7d37d8f
Artur Agh <aghartur@gmail.com>
2024-07-03 07:10:46 +0200

    test get blogs

--- src/routes/blog-router.ts
+++ src/routes/blog-router.ts
@@ -28,14 +28,16 @@ export const blogsRoute = Router();
 blogsRoute.get(
   "/",
   async (req: RequestWithQuery<BlogQueryInputModel>, res: Response) => {
-    const sortData = {
-      searchNameTerm: req.query.searchNameTerm ?? null,
-      sortBy: req.query.sortBy ?? "createdAt",
-      sortDirection: req.query.sortDirection ?? "desc",
-      pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
-      pageSize: req.query.pageSize ? +req.query.pageSize : 10,
-    };
-    const blogs = await BlogService.getAllBlogs(sortData);
+    // const sortData = {
+    //   searchNameTerm: req.query.searchNameTerm ?? null,
+    //   sortBy: req.query.sortBy ?? "createdAt",
+    //   sortDirection: req.query.sortDirection ?? "desc",
+    //   pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
+    //   pageSize: req.query.pageSize ? +req.query.pageSize : 10,
+    // };
+    // const blogs = await BlogService.getAllBlogs(sortData);
+
+    const blogs = ["a", "b", "c"];
     res.send(blogs).status(HTTP_RESPONSE_CODES.SUCCESS);
   }
 );
