c2814c0f594d294063c582f84476654e2a10fb5c
Artur Agh <aghartur@gmail.com>
2024-07-07 13:19:38 +0200

    remove changes in blog routes

--- src/routes/blog-router.ts
+++ src/routes/blog-router.ts
@@ -28,16 +28,15 @@ export const blogsRoute = Router();
 blogsRoute.get(
   "/",
   async (req: RequestWithQuery<BlogQueryInputModel>, res: Response) => {
-    // const sortData = {
-    //   searchNameTerm: req.query.searchNameTerm ?? null,
-    //   sortBy: req.query.sortBy ?? "createdAt",
-    //   sortDirection: req.query.sortDirection ?? "desc",
-    //   pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
-    //   pageSize: req.query.pageSize ? +req.query.pageSize : 10,
-    // };
-    // const blogs = await BlogService.getAllBlogs(sortData);
+    const sortData = {
+      searchNameTerm: req.query.searchNameTerm ?? null,
+      sortBy: req.query.sortBy ?? "createdAt",
+      sortDirection: req.query.sortDirection ?? "desc",
+      pageNumber: req.query.pageNumber ? +req.query.pageNumber : 1,
+      pageSize: req.query.pageSize ? +req.query.pageSize : 10,
+    };
+    const blogs = await BlogService.getAllBlogs(sortData);
 
-    const blogs = [1, 2, 3, 4];
     res.send(blogs).status(HTTP_RESPONSE_CODES.SUCCESS);
   }
 );
