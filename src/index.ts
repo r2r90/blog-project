f5c27cb56fcb4ef54914a57ac523e81e78d699d9
Artur Agh <aghartur@gmail.com>
2024-07-07 15:31:35 +0200

    vercel settings added - 1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { runDb } from "./db/db";
 import { appConfig } from "./config/config";
 
 const port = appConfig.PORT || 4000;
+app.set("trust proxy", true);
 
 app.listen(port, async () => {
   console.log(`LISTENING ON PORT: ${port}`);
